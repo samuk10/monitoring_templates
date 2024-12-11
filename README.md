49a02b279a6d2fb8b81aa8c68e7f65dd3fb95fcf
Samuel Sampaio <samukasampaio@hotmail.com>
2024-12-11 13:31:11 -0300

    chore: first commit, adding all stuffs

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# monitoring_templates
-Zabbix and Grafana Templates for monitoring
+# Nokia Alcatel + PPPoE Discovery Script
+
+### Contents:
+- Nokia Alcatel General
+- Nokia PPPoE discovery + Script
+- Nokia BGP
+
+### Using: 
+
+1. Put the `./src/externalscripts/nokia-pppoe.py` script in the `/opt/zabbix/externalscripts` directory
+2. Import both templates: `Alcatel Timetra TiMOS SNMP` and `Nokia Alcatel SNMPv2 - BGP`
+
+### Hint:
+You can also edit the name of the interfaces when zabbix create the itens:
+
+1. Go to: Alcatel Timetra TiMOS SNMP
+2. Itens > `Retorno Json - PPPoE`
+3. Create a processing:
+4. Replace: `{#IF_NAME}` with: `{#IF_NAME}(XPTO)`
+
+Example: `lag-1:120` to `lag-1:120(CIRCUITO-BNG-SAO_PAULO)`
+
+### Tested on:
+
+- zabbix 5.4
+- grafana 9.4
---
+++ src/externalscripts/nokia-pppoe.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+###################################################################################
+# Desenvolvido por Samuel Sampaio
+# Versão: 1.0
+###################################################################################
+
+import json
+import re
+import subprocess
+import sys
+
+host_ip = sys.argv[1]
+community = sys.argv[2]
+
+
+# Função para executar o snmpwalk e retornar as linhas de saída
+def execute_snmpwalk():
+    command = f"snmpbulkwalk -v2c -c {community} {host_ip} .1.3.6.1.4.1.6527.3.1.2.33.1.17.1.3"
+    try:
+        output = subprocess.check_output(
+            command, shell=True, stderr=subprocess.STDOUT, universal_newlines=True
+        )
+        return output.splitlines()
+    except subprocess.CalledProcessError as e:
+        print(f"Erro ao executar o comando: {e}")
+        return []
+
+
+# Executar o snmpwalk e capturar as linhas de saída
+lines = execute_snmpwalk()
+
+# Extrair os valores entre o primeiro "|" e o primeiro "."
+values = []
+for line in lines:
+    match = re.search(r"\|([^\.]+)", line)
+    if match:
+        value = match.group(1).strip()
+        values.append(value)
+
+# Contar as ocorrências de cada valor
+count_dict = {}
+for value in values:
+    if value not in count_dict:
+        count_dict[value] = 1
+    else:
+        count_dict[value] += 1
+
+# Preparar o resultado no formato JSON desejado
+result = [{"IF_NAME": name, "IF_COUNT": count} for name, count in count_dict.items()]
+
+# Imprimir o resultado
+print(json.dumps(result, indent=4))
